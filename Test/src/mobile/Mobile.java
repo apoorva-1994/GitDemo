7600b70739a871c41ad88911af77133800ae959f
apoorva-1994 <shanbhagapoorva100@gmail.com>
2024-01-02 09:54:04 -0800

    First commit

---
+++ Test/src/mobile/Mobile.java
@@ -0,0 +1,10 @@
+package mobile;
+
+public class Mobile {
+	
+	public static void name() {
+		
+		System.out.println("Mobile Test 1");
+	}
+
+}
---
+++ Test/src/web/Selenium.java
@@ -0,0 +1,11 @@
+package web;
+
+public class Selenium {
+	
+	public static void name() {
+		
+		System.out.println("Selenium Test 1");
+		
+	}
+
+}
